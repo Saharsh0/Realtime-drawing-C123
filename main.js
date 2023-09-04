6728cb388fa17d4676edb20e1e7cf0c7720aa300
Saharsh0 <113522933+Saharsh0@users.noreply.github.com>
2023-09-04 17:28:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX = 0;
+noseY = 0;
+wristL = 0;
+wristR = 0;
+side = 0;
+function setup()
+{
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    posenet = ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotPoses);
+}
+
+function draw()
+{
+    background('grey');
+    fill("red");
+    stroke("black")
+    square(noseX, noseY, side)
+    document.getElementById("pixel").innerHTML = "The side of the square measures"+side+"px"
+}
+
+function modelLoaded()
+{
+    console.log("model loaded");
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        wristL = results[0].pose.leftWrist.x;
+        wristR = results[0].pose.rightWrist.x;
+        side = floor(wristL - wristR);
+    }
+}
